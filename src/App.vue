151061e9ebec673cc17a135e9d7e3a031c548be5
Vlad7Stepanov7 <svlad4117@gmail.com>
2023-05-06 15:57:17 +0300

    added component StarRating and ApartamentsItem

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <ApartmentsItem
+  :description="apartment.description"
+  :price="apartment.price"
+  :rating="apartment.rating"
   
+  />
 </template>
 
 <script>
+import StarRating from './components/StarRating.vue';
+import ApartmentsItem from './components/apartment/ApartmentsItem.vue';
 
 export default {
   name: 'App',
   components: {
-
-  }
+    ApartmentsItem,
+    StarRating
+  },
+  data() {
+    return {
+      apartment: {
+        id: `1`,
+        title: "AAAAAAAAAAAAAA",
+        description: "BBBBBBBBBBBB",
+        price: 2200,
+        rating: 4.7,
+        location: {
+          city: "Dnipro",
+        },
+        owner: {
+          name: "Elien",
+          phone: "115-355-5652",
+          email: "Elien@gamil.com"
+        }
+      }
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+    <button class="btn" v-on="$listeners" v-bind:type="type">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+    name: "Button",
+    props: {
+        type: {
+            type: String,
+            default: 'button',
+            required: true
+        },
+        outlined: {
+            type: Boolean,
+            default: false
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.btn {
+    display: inline-block;
+    width: 220px;
+    height: 44px;
+    background: #FF662D;
+
+    &--outlined {
+        background-color: none;
+        border: 1px solid #FF6629;
+        color: #FF6629;
+    }
+}
+</style>
